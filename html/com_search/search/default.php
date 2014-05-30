7c83fb04ab14d67169458f9860fb4e7bbd09a424
zoearthmoon@gmail.com <zoearthmoon@gmail.com>
2014-05-30 16:58:29 +0800

    調整 調整搜尋版型

--- html/com_search/search/default.php
+++ html/com_search/search/default.php
@@ -14,45 +14,48 @@ JHtml::_('formbehavior.chosen', 'select');
 <?php endif; ?>
 
 
-<form id="searchForm" action="<?php echo JRoute::_('index.php?option=com_search');?>" method="post">
-    <div class="btn-toolbar">
-        <div class="btn-group pull-left">
-			<input type="text" name="searchword" placeholder="<?php echo JText::_('COM_SEARCH_SEARCH_KEYWORD'); ?>" id="search-searchword" size="30" maxlength="<?php echo $upper_limit; ?>" value="<?php echo $this->escape($this->origkeyword); ?>" class="inputbox" />
-		</div>
-		<div class="btn-group pull-left">
-			<button name="Search" onclick="this.form.submit()" class="btn hasTooltip" title="<?php echo JHtml::tooltipText('COM_SEARCH_SEARCH');?>"><i class="icon-search"></i></button>
-		</div>
-		<input type="hidden" name="task" value="search" />
-		<div class="clearfix"></div>
-	</div>
+<form id="searchForm" action="<?php echo JRoute::_('index.php?option=com_search');?>" method="post" class="form-horizontal" >
+    <div class="control-group">
+        <label class="control-label" for="inputEmail"><?php echo JText::_('COM_SEARCH_SEARCH_KEYWORD'); ?></label>
+        <div class="controls">
+            <div class="span3">
+            <input type="text" name="searchword" placeholder="<?php echo JText::_('COM_SEARCH_SEARCH_KEYWORD'); ?>" id="search-searchword" maxlength="<?php echo $upper_limit; ?>" value="<?php echo $this->escape($this->origkeyword); ?>" class="inputbox" />
+            </div>
+            <div class="span2" >
+            <button name="Search" onclick="this.form.submit()" class="btn hasTooltip" title="<?php echo JHtml::tooltipText('COM_SEARCH_SEARCH');?>"><i class="icon-search"></i></button>
+            </div>
+        </div>
+        <input type="hidden" name="task" value="search" />
+    </div>
 
-	<div class="searchintro<?php echo $this->params->get('pageclass_sfx'); ?>">
-		<?php if (!empty($this->searchword)):?>
-		<p><?php echo JText::plural('COM_SEARCH_SEARCH_KEYWORD_N_RESULTS', '<span class="badge badge-info">'. $this->total. '</span>');?></p>
-		<?php endif;?>
-	</div>
+    <div class="control-group">
+        <label class="control-label" for="inputEmail"></label>
+        <div class="controls">
+            <?php if (!empty($this->searchword)):?>
+            <p><?php echo JText::plural('COM_SEARCH_SEARCH_KEYWORD_N_RESULTS', '<b class="label label-success">'. $this->total. '</b>');?></p>
+            <?php endif;?>
+        </div>
+    </div>
 
-	<fieldset class="phrases">
-			<input type="hidden" value="all" id="searchphraseall" name="searchphrase">
-			<div class="ordering-box">
-			<label for="ordering" class="ordering">
-				<?php echo JText::_('COM_SEARCH_ORDERING');?>
-			</label>
-			<?php echo $this->lists['ordering'];?>
-			</div>
-	</fieldset>
+    <div class="control-group">
+        <label class="control-label" for="inputEmail"><?php echo JText::_('COM_SEARCH_ORDERING');?></label>
+        <div class="controls">
+            <?php echo $this->lists['ordering'];?>
+        </div>
+    </div>
+
     <input type="hidden" id="area-z2" value="z2" name="areas[]">
 
     <?php if ($this->total > 0) : ?>
-	<div class="form-limit">
-		<label for="limit">
-			<?php echo JText::_('JGLOBAL_DISPLAY_NUM'); ?>
-		</label>
-		<?php echo $this->pagination->getLimitBox(); ?>
-	</div>
-    <p class="counter">
-		<?php echo $this->pagination->getPagesCounter(); ?>
-	</p>
+    <div class="control-group">
+        <label class="control-label" for="inputEmail"><?php echo JText::_('JGLOBAL_DISPLAY_NUM'); ?></label>
+        <div class="controls">
+            <?php echo $this->pagination->getLimitBox(); ?>
+        </div>
+        <div class="controls">
+            <?php echo $this->pagination->getPagesCounter(); ?>
+        </div>
+    </div>
     <?php endif; ?>
 
 </form>
@@ -63,9 +66,6 @@ JHtml::_('formbehavior.chosen', 'select');
 <?php endif; ?>
 
 <?php if ($this->error == null && count($this->results) > 0) :?>
-
-
-
 
 <div class="search-results<?php echo $this->pageclass_sfx; ?> row">
 <?php foreach ($this->results as $result) : ?>
@@ -95,10 +95,6 @@ JHtml::_('formbehavior.chosen', 'select');
 <div class="pagination">
 	<?php echo $this->pagination->getPagesLinks(); ?>
 </div>
-
-
-
-
 
 <?php endif; ?>
 </div>
